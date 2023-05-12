ebf10143098e93e780eaee5b31f5b3acd0f9d250
2132175 <133364122+2132175@users.noreply.github.com>
2023-05-12 15:42:28 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+ 
+<br>
+<?php
+
+$int1 = 10;
+$int2 = 12;
+
+$int1 * $int2;
+echo $int1+$int2;
+?>
+
+<br>
+<?php
+$nummer= 10;
+$num= 12;
+echo $nummer*$num;
+?>
+<br>
+<?php
+$naam = "Mohamed Karimi";
+$tekst = "welkom Mohamed";
+echo $tekst;
+
+?>
+<?php
+
+// deze code bevat veel fouten haal alle fouten eruit!
+//
+
+$mijnNaam="Carl";
+$jouwNaam="Camel";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!"
+
+?>
